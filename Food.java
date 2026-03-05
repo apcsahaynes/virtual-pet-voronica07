7e7e7ef01dfe30de05875bf62676dacaa08f1979
voronica07 <veronica.chen@jpschools.org>
2026-03-05 10:11:28 -0600

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,29 @@
 public class Food {
+    private String name;
+    private int energyIncrease;
+    private int happinessIncrease;
+    private int weightGain;
     
+ public Food(String name, int energyIncrease, int happinessIncrease, 
+ int weightGain){
+     this.name = name;
+     this.energyIncrease = energyIncrease;
+     this.happinessIncrease = happinessIncrease;
+     this.weightGain = weightGain;
+ }
+ public String getName(){
+     return this.name;
+ }
+ 
+ public int getEnergyIncrease(){
+     return this.energyIncrease;
+ }
+ 
+ public int getHappinessIncrease(){
+     return this.happinessIncrease;
+ }
+
+ public int getWeightGain(){
+     return this.weightGain;
+ }
 }
