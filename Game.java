d5522e5760c083ce881350b0dabd98a96a8539cf
voronica07 <veronica.chen@jpschools.org>
2026-03-05 10:11:56 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,28 @@
 public class Game {
-    
+   private String name;
+   private int happinessIncr;
+   private int weightDecr;
+
+    public Game(String name, int happinessIncr, int weightDecr){
+    this.name = name;
+    this.happinessIncr = happinessIncr;
+    this.weightDecr = weightDecr;
+    }
+
+   public String getName(){
+       return this.name;
+   }
+
+   public int getHappinessIncr(){
+       return this.happinessIncr;
+   }
+
+   public int getWeightDecr(){
+       return this.weightDecr;
+   }
+   
+ public boolean isWinner() {
+     double randNum = Math.floor(Math.random() * 2);
+     return randNum == 1;
+ }
 }
