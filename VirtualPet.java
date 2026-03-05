4bdfe1214b2b440d0b5cf121a80147d0691f75fd
voronica07 <veronica.chen@jpschools.org>
2026-03-05 10:12:28 -0600

    Update VirtualPet.java

--- VirtualPet.java
+++ VirtualPet.java
@@ -1,3 +1,68 @@
-public class VirtualPet {
-    
+public class VirtualPet4 {
+  private String name;
+  private int energy;
+  private int weight;
+  private int happiness;
+  private int ageMonths;
+  private int ageYears;
+
+  public VirtualPet4(String name){
+    this.name = name;
+    this.energy = 0;
+    this.weight = 5;
+    this.happiness = 0;
+    this.ageMonths = 0;
+    this.ageYears = 0;
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+  public int getEnergyLevel(){
+    return energy;
+  }
+
+  public int getHappinessLevel(){
+    return happiness;
+  }
+
+  public void feed(Food food){
+   this.energy += food.getEnergyIncrease();
+   this.weight += food.getWeightGain();
+   this.happiness += food.getHappinessIncrease();
+  }
+
+  public void play(Game game){
+    if (game.isWinner()) {
+      this.happiness += game.getHappinessIncr();
+      this.happiness = Math.max(Math.min(this.happiness, 10), 0);
+      this.weight -= game.getWeightDecr();
+      this.weight = Math.max(this.weight, 5);
+    }
+  }
+
+  public void updateStatus(){
+    if (happiness > 0){
+      happiness--;
+    }
+    if (energy > 0){
+      energy--;
+    }
+    ageMonths++;
+    ageConverter();
+  }
+
+  private void ageConverter(){
+    if (ageMonths >= 12){
+      ageYears++;
+      ageMonths = 0;
+    }
+  } 
+
+  public String toString(){
+    return name + "'s information: \nEnergy: " + energy 
+    + "\nWeight: " + weight + "\nHappiness: " + happiness
+    + "\nAge: " + ageYears + " years and " + ageMonths + " months.";
+  }
 }
