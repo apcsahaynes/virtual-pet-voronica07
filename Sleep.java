ec7ea92c58ec329daa869e420504b9f12b6c8886
voronica07 <veronica.chen@jpschools.org>
2026-03-09 10:04:39 -0500

    Add files via upload

---
+++ Sleep.java
@@ -0,0 +1,16 @@
+public class Sleep {
+    private String name;
+    private int energyIncrease;
+    
+    public Sleep(String name, int energyIncrease){
+     this.name = name;
+     this.energyIncrease = energyIncrease;
+    }
+    public String getName(){
+       return this.name;
+    }
+       
+    public int getEnergyIncrease(){
+       return this.energyIncrease;
+}
+}
